9104fa11c0ed677eeb4c1876190b3663906bd2e9
mateuszdylewski <mateuszdylewski@yahoo.com>
2022-11-06 18:30:56 +0100

    Initial Commit

---
+++ SMB/SMBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMBApp.swift
+//  SMB
+//
+//  Created by Mateusz Dylewski on 06/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SMBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
